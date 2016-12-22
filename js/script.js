9ebd41093de4e1e3b5afd703ea58f32c1c6f4500
mattbfishman <mbfishman@quinnipiac.edu>
2016-12-22 02:09:15 -0500

    Added weclome screen

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    
+    setInterval ('cursorAnimation()', 700);
+
+});
+    
+function cursorAnimation() {
+$('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
+
+$('.enter-link').click(function () {
+    $(this).parent().parent().parent().parent().parent().fadeOut(800);
+});
