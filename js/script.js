da4ab6decc7fa46434074378a1ea484796b9c152
mattbfishman <mbfishman@quinnipiac.edu>
2016-12-22 22:03:13 -0500

    Add icons

--- js/script.js
+++ js/script.js
@@ -15,9 +15,10 @@ $('#cursor').animate({
 $('.enter-link').click(function () {
 	$(".terminal-container-fluid").css({ 'display' : 'block'});
 	$(".projects-container-fluid").css({ 'display' : 'block'});
-	$(".projects-container-fluid").css({ 'display' : 'block'});
+	$(".skills-container-fluid").css({ 'display' : 'block'});
 	$(".footer").css({ 'display' : 'block'});
 	$(".social-container-fluid").css({ 'display' : 'block'});
 	$("#custom-bootstrap-menu").css({ 'display' : 'block'});
     $(this).parent().parent().parent().parent().parent().fadeOut(800);
 });
+
